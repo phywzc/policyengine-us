837fef371c96db0c636ebf7140343abb65580608
phywzc <97962358+phywzc@users.noreply.github.com>
2023-08-14 19:19:07 +0000

    Colorado Innovative Motor Vehicle Credit and Innovative Truck Credit Fixes #2831

---
+++ policyengine_us/variables/household/expense/vehicle/innovative_motor_vehicle_fuel_type.py
@@ -0,0 +1,16 @@
+from policyengine_us.model_api import *
+
+
+class InnovativeMotorVehicleFuelType(Enum):
+    ELECTRIC_VECHICLE = "Electric Vehicle"
+    PLUG_IN_HYBRID_ELECTRIC_VECHICLE= "Plug-in Hybrid Electric Vehicle"
+    OTHER = "Other"
+
+
+class innovative_motor_vehicle_fuel_type(Variable):
+    value_type = Enum
+    possible_values = InnovativeMotorVehicleFuelType
+    default_value = InnovativeMotorVehicleFuelType.OTHER
+    entity = TaxUnit
+    label = "Colorado innovative motor vechicle fuel type "
+    definition_period = YEAR
